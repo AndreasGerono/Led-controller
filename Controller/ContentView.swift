19459e2eec94d09e3f2e8d0335fcf3067254873a
AndreasGerono <andreas.gerono@gmail.com>
2021-05-04 19:46:40 +0200

    Initial Commit

---
+++ Controller/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Controller
+//
+//  Created by Andreas Gerono on 04/05/2021.
+//  Copyright © 2021 Andreas Gerono. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
